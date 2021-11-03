c92d4b7882a7f216b60545195614aa6cc184e351
Cameron <561860+wallies@users.noreply.github.com>
2021-11-04 09:13:22 +1000

    update score logic for nist
    https://www.vericlouds.com/nist-password-guidelines-2021-challenging-traditional-password-management/
    https://auth0.com/blog/dont-pass-on-the-new-nist-password-guidelines/#1--Length---Complexity

--- src/logic/scorePassword.js
+++ src/logic/scorePassword.js
@@ -1,48 +1,24 @@
 const scorePassword = pass => {
   let score = 0
   let length = 0
-  let specialChar = 0
-  let caseMix = 0
-  let numCharMix = 0
 
-  const specialCharRegex = /[^A-Za-z0-9]/g
-  const lowercaseRegex = /(.*[a-z].*)/g
-  const uppercaseRegex = /(.*[A-Z].*)/g
-  const numberRegex = /(.*[0-9].*)/g
   const repeatCharRegex = /(\w)(\1+\1+\1+\1+)/g
-
-  const hasSpecialChar = specialCharRegex.test(pass)
-  const hasLowerCase = lowercaseRegex.test(pass)
-  const hasUpperCase = uppercaseRegex.test(pass)
-  const hasNumber = numberRegex.test(pass)
   const hasRepeatChars = repeatCharRegex.test(pass)
 
   if (pass.length > 4) {
-    if ((hasLowerCase || hasUpperCase) && hasNumber) {
-      numCharMix = 1
-    }
-
-    if (hasUpperCase && hasLowerCase) {
-      caseMix = 1
-    }
-
-    if ((hasLowerCase || hasUpperCase || hasNumber) && hasSpecialChar) {
-      specialChar = 1
-    }
-
     if (pass.length > 8) {
       length = 1
     }
 
-    if (pass.length > 12 && !hasRepeatChars) {
+    if (pass.length > 15) {
       length = 2
     }
 
-    if (pass.length > 25 && !hasRepeatChars) {
-      length = 3
+    if (pass.length > 25 ) {
+      length = 4
     }
 
-    score = length + specialChar + caseMix + numCharMix
+    score = length
 
     if (score > 4) {
       score = 4
